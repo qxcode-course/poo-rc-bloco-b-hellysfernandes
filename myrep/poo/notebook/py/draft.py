634ac26c6602f66c4ab8b9ffbe5e2f5fe6dfde1e
Hellys Fernandes <hellys fernandes>
2025-10-25 23:10:54 -0300

    incremento da bateria no notebook

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -1,6 +1,31 @@
+class Bateria:
+    def __init__(self, capacidade: int = 0):
+        self.__capacidade: int = capacidade
+        self.__carga: int = capacidade
+
+    def getCapacidade(self) -> int:
+        return self.__capacidade
+    
+    def getCarga(self) -> int:
+        return self.__carga
+    
+    def setBateriInit(self, batInit: int) -> None:
+        self.__capacidade = batInit
+        self.__carga = batInit
+
+    def setDecrement(self, decrement: int) -> None:
+        self.__carga -= decrement
+    
+    def __str__(self) -> str:
+        return f"({self.__carga}/{self.__capacidade})"
+
 class Notebook:
+
+    bateria = Bateria()
+
     def __init__(self):
         self.__ligado: bool = False
+        self.__bateria: Bateria | None = None
 
     def getLigado(self) -> bool:
         return self.__ligado
@@ -11,6 +36,9 @@ class Notebook:
         return "ligado"
     
     def setLigar(self) -> None:
+        if self.__bateria == None:
+            print("não foi possível ligar")
+            return 
         self.__ligado = True
 
     def setDesligar(self) -> None:
@@ -18,10 +46,21 @@ class Notebook:
 
     def setUsar(self, usar) -> str:
         if self.__ligado == True:
-            print(f"usado por {usar} minutos")
-            return
+            if usar <= self.bateria.__carga:
+                self.bateria.setDecrement(usar)
+                print(f"usado por {usar} minutos")
+                return
+            return "erro no if doido"
         print("erro: ligue o notebook primeiro")
+
+    def setBateri(self) -> str:
+        if self.__bateria == None:
+            return "Nenhuma"
+        return f"{self.__bateria}"
     
+    def setBateriaIncluida(self) -> None:
+        self.__bateria =  self.bateria
+
 def main():
 
     notebook = Notebook()
@@ -35,7 +74,7 @@ def main():
         if args[0] == "end":
             break
         elif args[0] == "show":
-            print(f"status: {notebook.setStatus()}")
+            print(f"status: {notebook.setStatus()}, batrtia: {notebook.setBateri()}")
         elif args[0] == "ligar":
             notebook.setLigar()
         elif args[0] == "desligar":
@@ -43,4 +82,12 @@ def main():
         elif args[0] == "usar":
             usar: int = int(args[1])
             notebook.setUsar(usar)
+        elif args[0] == "bateria":
+            batInit: int = int(args[1])
+            notebook.bateria.setBateriInit(batInit)
+        elif args[0] == "mostrar":
+            print(notebook.bateria)
+        elif args[0] == "incluir":
+            notebook.setBateriaIncluida()
+
 main()
